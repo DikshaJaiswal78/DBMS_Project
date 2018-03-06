143aab8e8570960d217f5495af0b808f5ba82888
Mr. Lazy <abhi.jadav1@gmail.com>
2018-03-06 01:34:55 -0800

    Trying JPanel grid layout.

--- Yelp Application/src/MainJframe.java
+++ Yelp Application/src/MainJframe.java
@@ -1,6 +1,11 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -14,15 +19,58 @@ public class MainJframe extends JFrame {
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+			private JPanel controlPanel;
+			private GridLayout layout;
+			private AbstractButton panel;
+			private MainJframe mainFrame;
+
 			public void run() {
 				try {
-					MainJframe frame = new MainJframe();
-					frame.setVisible(true);
+					mainFrame = new MainJframe();
+
+					controlPanel = new JPanel();
+					controlPanel.setLayout(new FlowLayout());
+					layout = new GridLayout(0, 3);
+					layout.setHgap(10);
+					layout.setVgap(10);
+
+					JPanel panel = new JPanel();
+					// panel.setBackground(Color.darkGray);
+					// panel.setSize(300, 300);
+					panel.setLayout(layout);
+
+					JPanel jpanel1 = new JPanel();
+
+					jpanel1.setBackground(Color.BLACK);
+					panel.add(jpanel1);
+
+					jpanel1.setBackground(Color.YELLOW);
+					panel.add(jpanel1);
+
+					jpanel1.setBackground(Color.BLUE);
+					panel.add(jpanel1);
+
+					jpanel1.setBackground(Color.GREEN);
+					panel.add(jpanel1);
+
+					jpanel1.setBackground(Color.ORANGE);
+					panel.add(jpanel1);
+
+					controlPanel.add(panel);
+
+					mainFrame.add(controlPanel);
+
+					mainFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+	}
+
+	private void createJPanes() {
+		// TODO Auto-generated method stub
+
 	}
 
 	/**
